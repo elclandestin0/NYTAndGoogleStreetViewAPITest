6d1eccb9eab05f140b0730157299e9bcdddf3abe
mk <majd.khoury93@gmail.com>
2018-02-04 15:40:11 -0500

    Integrated NYT API feat: Parsed an article from JSON request to the NYT with the cityElement attached to the API request

--- js/script.js
+++ js/script.js
@@ -11,13 +11,33 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    var cityElement = $("#street").val();
-    var streetElement = $("#city").val();
+    // Google street view Request
+    var cityElement = $("#city").val();
+    var streetElement = $("#street").val();
     var address = streetElement + ', ' + cityElement;
     var googleViewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address +'';
     $body.append(
       '<img class ="bgimg" src="' + googleViewURL + '">"');
+
+
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+      url += '?' + $.param({
+        'api-key': "d7cb30577300448dbaee5c6fec97599b",
+        'q': cityElement
+      });
+
+    $.getJSON(url, function(data){
+      console.log(data['response']['docs'][0]['web_url']);
+      // $.each(data, function(key, val){
+      //   articles.push("<li id =")
+      // })
+    });
     return false;
-};
+    };
+
+    // NYT article request
+
+
+
 
 $('#form-container').submit(loadData);
